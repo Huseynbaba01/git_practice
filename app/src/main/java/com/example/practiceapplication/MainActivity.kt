5815a9cc5508d1f680c8a03c391ef5158032cf27
Huseynbaba01 <hrzaquliyev@std.beu.edu.az>
2021-06-04 16:05:33 +0400

    My first commit here

---
+++ app/src/main/java/com/example/practiceapplication/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.practiceapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val textView = findViewById<TextView>(R.id.text)
+// ...
+
+// Instantiate the RequestQueue.
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://projects.vishnusivadas.com/testing/read.php"
+
+        val stringRequest = StringRequest(Request.Method.GET, url,
+                Response.Listener<String> { response ->
+                    textView.text = "Response is: $response"
+                },
+                Response.ErrorListener { textView.text = "That didn't work!" })
+
+        queue.add(stringRequest)
+    }
+}
