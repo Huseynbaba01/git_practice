eb51e93f40c46c0b3fd90096d41476bb47779e6f
Huseynbaba01 <hrzaquliyev@std.beu.edu.az>
2021-06-05 23:50:42 +0400

    any commit here

--- app/src/main/java/com/example/practiceapplication/MainActivity.kt
+++ app/src/main/java/com/example/practiceapplication/MainActivity.kt
@@ -1,20 +1,47 @@
 package com.example.practiceapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.example.practiceapplication.ViewModelClasses.MainActivityViewModel
+import com.example.practiceapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var viewModel:MainActivityViewModel
+    lateinit var button: Button
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val textView = findViewById<TextView>(R.id.text)
-// ...
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding.editText.setText(intent.getStringExtra("text"))
+        button = binding.button
+        button.setOnClickListener {
+            val intent:Intent = Intent(applicationContext,MainActivity::class.java)
+            startActivity(intent
+                .putExtra("text",binding.editText.text.toString()))
+        }
 
+
+
+
+        val textView = findViewById<TextView>(R.id.text)
+        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+        viewModel.currentText.observe(this, Observer {
+
+        })
+// ...
+/*
 // Instantiate the RequestQueue.
         val queue = Volley.newRequestQueue(this)
         val url = "https://projects.vishnusivadas.com/testing/read.php"
@@ -25,7 +52,7 @@ class MainActivity : AppCompatActivity() {
                 },
                 Response.ErrorListener { textView.text = "That didn't work!" })
 
-// Add the request to the RequsetQueue.
-        queue.add(stringRequest)
+// Add the request to the RequestQueue.
+        queue.add(stringRequest)*/
     }
 }
--- app/src/main/java/com/example/practiceapplication/SecondActivity.kt
+++ app/src/main/java/com/example/practiceapplication/SecondActivity.kt
@@ -1,13 +1,26 @@
 package com.example.practiceapplication
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.practiceapplication.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity(){
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
-
+    lateinit var binding:ActivitySecondBinding
+    lateinit var button: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_second)
+        binding.editText.setText(intent.getStringExtra("text"))
+        binding.button.setOnClickListener {
+            val intent:Intent = Intent(applicationContext,SecondActivity::class.java)
+            startActivity(intent
+                .putExtra("text",binding.editText.text.toString()))
+        }
 
 
 
